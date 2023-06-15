aa53b7ab345cbb177eae2c35172d73b9cb2102df
Risttahukas <kassjuss@gmail.com>
2023-06-15 18:07:45 +0100

    added creative tab

--- src/main/java/net/risttahukas/moremobheadsmod/MoreMobHeadsMod.java
+++ src/main/java/net/risttahukas/moremobheadsmod/MoreMobHeadsMod.java
@@ -1,6 +1,7 @@
 package net.risttahukas.moremobheadsmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -10,6 +11,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.risttahukas.moremobheadsmod.item.ModCreativeModeTabs;
+import net.risttahukas.moremobheadsmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -24,6 +27,9 @@ public class MoreMobHeadsMod
     public MoreMobHeadsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -36,7 +42,15 @@ public class MoreMobHeadsMod
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTab() == ModCreativeModeTabs.MOB_HEADS_TAB.get()) {
+            event.accept(Items.SKELETON_SKULL);
+            event.accept(Items.WITHER_SKELETON_SKULL);
+            event.accept(Items.PLAYER_HEAD);
+            event.accept(Items.ZOMBIE_HEAD);
+            event.accept(Items.CREEPER_HEAD);
+            event.accept(Items.PIGLIN_HEAD);
+            event.accept(Items.DRAGON_HEAD);
+        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
