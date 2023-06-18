74ddb4b33e78648bf521b06e456ec006f0e90c83
Risttahukas <kassjuss@gmail.com>
2023-06-18 17:02:36 +0100

    added data generators for loot tables, item models, blockstates

---
+++ src/main/java/net/risttahukas/moremobheads/datagen/ModBlockStateProvider.java
@@ -0,0 +1,35 @@
+package net.risttahukas.moremobheads.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.AbstractSkullBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.risttahukas.moremobheads.MoreMobHeadsMod;
+import net.risttahukas.moremobheads.block.ModBlocks;
+
+import java.util.Iterator;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    private static final ResourceLocation skullModelLocation = new ResourceLocation("minecraft", "block/skull");
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, MoreMobHeadsMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        Iterator<RegistryObject<Block>> iterator = ModBlocks.BLOCKS.getEntries().stream().iterator();
+        while (iterator.hasNext()) {
+            RegistryObject<Block> block = iterator.next();
+            if (block.get() instanceof AbstractSkullBlock) {
+                skull(block);
+            }
+        }
+    }
+
+    public void skull(RegistryObject<Block>  block) {
+        simpleBlock(block.get(), models().getExistingFile(skullModelLocation));
+    }
+}
---
+++ src/main/java/net/risttahukas/moremobheads/datagen/loot/ModBlockLootTables.java
@@ -0,0 +1,47 @@
+package net.risttahukas.moremobheads.datagen.loot;
+
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraftforge.registries.RegistryObject;
+import net.risttahukas.moremobheads.block.EffectSkullBlock;
+import net.risttahukas.moremobheads.block.ModBlocks;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+    public ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        Iterator<RegistryObject<Block>> iterator = ModBlocks.BLOCKS.getEntries().stream().iterator();
+        while (iterator.hasNext()) {
+            RegistryObject<Block> block = iterator.next();
+            if (block.get() instanceof EffectSkullBlock) {
+                dropSelfExplosionProof(block);
+            }
+        }
+    }
+
+    @Override
+    protected @NotNull Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
+
+    protected void dropSelfExplosionProof(RegistryObject<Block> block) {
+        this.dropOtherExplosionProof(block.get(), block.get());
+    }
+
+    protected void dropOtherExplosionProof(Block block, ItemLike itemLike) {
+        this.add(block, LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(LootItem.lootTableItem(itemLike))));
+    }
+}
