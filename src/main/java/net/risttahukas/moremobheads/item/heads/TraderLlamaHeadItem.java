581e1d711cc4b98bc22530b80eb269f3265934b7
Risttahukas <kassjuss@gmail.com>
2023-07-31 01:48:38 +0300

    removed redundant trader effect

--- src/main/java/net/risttahukas/moremobheads/item/heads/TraderLlamaHeadItem.java
+++ src/main/java/net/risttahukas/moremobheads/item/heads/TraderLlamaHeadItem.java
@@ -1,10 +1,8 @@
 package net.risttahukas.moremobheads.item.heads;
 
-import com.google.common.collect.ImmutableList;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Block;
 import net.risttahukas.moremobheads.effect.AbstractActiveHeadEffect;
-import net.risttahukas.moremobheads.effect.AbstractPassiveHeadEffect;
 import net.risttahukas.moremobheads.effect.HeadEffects;
 
 public class TraderLlamaHeadItem extends LlamaHeadItem {
